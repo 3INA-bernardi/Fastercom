3b2df0d1733dcea694dbea1f70d182ec6ae8fa14
5-INA_faes <146919404+5INA-faes@users.noreply.github.com>
2026-04-15 09:55:13 +0200

    Update dashboard_insegnante.php

---
+++ dashboard_insegnante.php
@@ -0,0 +1,54 @@
+<?php
+require_once "components/session.php";
+
+if($_SESSION["ruolo"] !== "Docente"){
+    header("Location: login.php"); 
+}
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css" >
+    <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <header>
+        <h1>Liceo P. Lodron</h1>
+    </header>
+    <?php require_once "components/navbar.php"; ?>
+    <main>
+<div class="btn-group">
+  <button type="button" class="btn btn-danger">Classi</button>
+  <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+    <span class="visually-hidden">Toggle Dropdown</span>
+  </button>
+  <?php 
+    require_once "db/dbutils.php";
+    classiDocente();
+  ?>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#"></a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+</div>
+    </main>
+    <?php require_once "components/footer.php"; ?>
+</body>
+
+</html>
