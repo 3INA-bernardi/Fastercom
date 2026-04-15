68400285e878b0d2edcee3bd4a6bb3d89b75b034
3INA-bernardi <146919145+3INA-bernardi@users.noreply.github.com>
2026-04-15 10:17:44 +0200

    inserimento navbar

---
+++ components/navbar.php
@@ -0,0 +1,11 @@
+<header class="site-header">
+    <div class="header-title">
+        <h1>Liceo P. Lodron</h1>
+        <p>Accesso rapido alle tue pagine</p>
+    </div>
+
+    <nav class="main-nav">
+        <a href="dashboard_studente.php">Dashboard</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+</header>
